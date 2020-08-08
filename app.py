e6088415404ac683e52ee8229958d78ad636de0a
Pedro Escoboza <psebastian01@hotmail.com>
2020-08-07 20:44:42 -0700

    Added nltk data and converted chatbot into REST API.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, request
 from flask_marshmallow import Marshmallow
 from chatbot import Chatbot
+from datetime import datetime
 
 app = Flask(__name__)
 chatbot = Chatbot()
@@ -11,11 +12,15 @@ chatbot = Chatbot()
 def index():
     return "<h1>HELLO</h1>"
 
-@app.route("/rest", methods=["GET"])
+@app.route("/chatbot", methods=["GET"])
 def get():
-    text = request.args.get("text")
+    text = request.args.get("msg")
     response = chatbot.chat(text)
-    return jsonify({"msg":response})
+    return jsonify({
+        "msg": text,
+        "chatbot_response":response,
+        "timestamp": str(datetime.now())
+        })
 
 if __name__=="__main__":
     app.run()
